4333ed19d4cc6ac0f58cb4c125ddd463996b39bc
dalmo <mffacuri@terra.com.br>
2021-09-27 12:44:36 -0300

    Primeiro commit springboot

---
+++ novoprojetopessoa/src/main/java/com/springboot/novoprojetopessoa/NovoprojetopessoaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.novoprojetopessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoprojetopessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoprojetopessoaApplication.class, args);
+	}
+
+}
---
+++ novoprojetopessoa/src/test/java/com/springboot/novoprojetopessoa/NovoprojetopessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.novoprojetopessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoprojetopessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
